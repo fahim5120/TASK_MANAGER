fdad6c86d55bb47e3580ea9540c78fd2c99bfe32
fahim5120 <muhdfahim786@gmail.com>
2025-11-06 10:34:00 +0530

    Remove unused style.css import
    Removed unused style.css import from main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './style.css' 
+ 
 import { BrowserRouter as Router } from "react-router-dom"
 import App from './App.jsx'
 
